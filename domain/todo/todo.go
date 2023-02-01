ec314411ee5ab92f32b3c9e8906d10934a079563
Chris James <quii@hey.com>
2023-02-01 09:10:50 +0000

    some unit tests

--- domain/todo/todo.go
+++ domain/todo/todo.go
@@ -15,14 +15,6 @@ type Todo struct {
 	Complete    bool
 }
 
-func (s *List) Rename(id uuid.UUID, name string) Todo {
-	i := slices.IndexFunc(s.todos, func(todo Todo) bool {
-		return todo.ID == id
-	})
-	s.todos[i].Description = name
-	return s.todos[i]
-}
-
 type List struct {
 	todos []Todo
 }
@@ -35,16 +27,20 @@ func (s *List) Add(description string) {
 	})
 }
 
-func (s *List) ToggleDone(id uuid.UUID) Todo {
-	i := slices.IndexFunc(s.todos, func(todo Todo) bool {
-		return todo.ID == id
-	})
-	s.todos[i].Complete = !s.todos[i].Complete
+func (s *List) Rename(id uuid.UUID, name string) Todo {
+	i := s.indexOf(id)
+	s.todos[i].Description = name
 	return s.todos[i]
 }
 
 func (s *List) Todos() []Todo {
 	return s.todos
+}
+
+func (s *List) ToggleDone(id uuid.UUID) Todo {
+	i := s.indexOf(id)
+	s.todos[i].Complete = !s.todos[i].Complete
+	return s.todos[i]
 }
 
 func (s *List) Delete(id uuid.UUID) {
@@ -77,12 +73,12 @@ func (s *List) Search(search string) []Todo {
 	return results
 }
 
+func (s *List) Get(id uuid.UUID) Todo {
+	return s.todos[s.indexOf(id)]
+}
+
 func (s *List) indexOf(id uuid.UUID) int {
 	return slices.IndexFunc(s.todos, func(todo Todo) bool {
 		return todo.ID == id
 	})
-}
-
-func (s *List) Get(id uuid.UUID) Todo {
-	return s.todos[s.indexOf(id)]
 }
--- domain/todo/todo_test.go
+++ domain/todo/todo_test.go
@@ -32,4 +32,82 @@ func TestService(t *testing.T) {
 		todos = service.Todos()
 		assert.False(t, todos[0].Complete)
 	})
+
+	t.Run("rename", func(t *testing.T) {
+		service := todo.List{}
+		service.Add("kill react")
+		service.Add("blah blah 1")
+
+		todos := service.Todos()
+		assert.Equal(t, "kill react", todos[0].Description)
+		id := todos[0].ID
+
+		service.Rename(id, "kill react and redux")
+
+		todos = service.Todos()
+		assert.Equal(t, "kill react and redux", todos[0].Description)
+		assert.Equal(t, "blah blah 1", todos[1].Description)
+	})
+
+	t.Run("delete", func(t *testing.T) {
+		service := todo.List{}
+		service.Add("kill react")
+		service.Add("blah blah 1")
+		service.Add("blah blah 2")
+		service.Add("blah blah 3")
+
+		todos := service.Todos()
+		assert.Equal(t, 4, len(todos))
+		id := todos[0].ID
+
+		service.Delete(id)
+
+		todos = service.Todos()
+		assert.Equal(t, 3, len(todos))
+		assert.Equal(t, "blah blah 1", todos[0].Description)
+		assert.Equal(t, "blah blah 2", todos[1].Description)
+		assert.Equal(t, "blah blah 3", todos[2].Description)
+	})
+
+	t.Run("reorder", func(t *testing.T) {
+		service := todo.List{}
+		service.Add("kill react")
+		service.Add("blah blah 1")
+		service.Add("blah blah 2")
+		service.Add("blah blah 3")
+
+		todos := service.Todos()
+		assert.Equal(t, 4, len(todos))
+		assert.Equal(t, "kill react", todos[0].Description)
+
+		// reorder
+		service.ReOrder([]string{
+			todos[3].ID.String(),
+			todos[2].ID.String(),
+			todos[1].ID.String(),
+			todos[0].ID.String(),
+		})
+
+		todos = service.Todos()
+		assert.Equal(t, 4, len(todos))
+		assert.Equal(t, "blah blah 3", todos[0].Description)
+		assert.Equal(t, "blah blah 2", todos[1].Description)
+		assert.Equal(t, "blah blah 1", todos[2].Description)
+		assert.Equal(t, "kill react", todos[3].Description)
+	})
+
+	t.Run("search", func(t *testing.T) {
+		service := todo.List{}
+		service.Add("kill react")
+		service.Add("blah blah 1")
+		service.Add("blah blah 2")
+		service.Add("blah blah 3")
+
+		todos := service.Search("blah")
+		assert.Equal(t, 3, len(todos))
+		assert.Equal(t, "blah blah 1", todos[0].Description)
+		assert.Equal(t, "blah blah 2", todos[1].Description)
+		assert.Equal(t, "blah blah 3", todos[2].Description)
+	})
+
 }
